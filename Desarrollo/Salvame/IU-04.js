cff1a570c91ea53ed86e674a42ebe6d391fd31f0
unknown <milena.mitac@unmsm.edu.pe>
2022-10-23 12:07:15 -0500

    IU-04
    Paso 1 Modulo de alerta

---
+++ Desarrollo/Salvame/IU-04.js
@@ -0,0 +1,49 @@
+const botonAnterior = document.querySelectorAll(".boton-anterior");
+const botonSiguiente = document.querySelectorAll(".boton-siguiente");
+const progreso = document.getElementById("progreso");
+const pasosFormulario = document.querySelectorAll(".paso-formulario");
+const pasosProgreso = document.querySelectorAll(".paso-progreso");
+
+let cantidadPasos = 0;
+
+botonSiguiente.forEach((boton) => {
+    boton.addEventListener("click", () => {
+        cantidadPasos++;
+        actualizarPasosFormulario();
+        actualizarBarraProgreso();
+    });
+});
+
+botonAnterior.forEach((boton) => {
+    boton.addEventListener("click", () => {
+        cantidadPasos--;
+        actualizarPasosFormulario();
+        actualizarBarraProgreso();
+    });
+});
+
+function actualizarPasosFormulario() {
+    pasosFormulario.forEach((paso) => {
+        paso.classList.contains("paso-formulario-activo") &&
+        paso.classList.remove("paso-formulario-activo");
+    });
+
+    pasosFormulario[cantidadPasos].classList.add("paso-formulario-activo");
+}
+
+function actualizarBarraProgreso() {
+    pasosProgreso.forEach((pasosProgreso, indice) => {
+        if(indice < cantidadPasos +1) {
+            pasosProgreso.classList.add("paso-progreso-activo");
+        } else {
+            pasosProgreso.classList.remove("paso-progreso-activo");
+        }
+        console.log(indice)
+    });
+
+    const progresoActivo = document.querySelectorAll(".paso-progreso-activo");
+
+    progreso.style.width = ((progresoActivo.length - 1) / (pasosFormulario.length - 1)) * 100 + "%";
+}
+
+console.log(cantidadPasos)
