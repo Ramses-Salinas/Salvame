cfe8d0b783079af31c64112c553e3a3a4ab7bb5a
Ramses <ramses.salinas.mejias@gmail.com>
2022-10-24 04:56:36 -0500

    Primera version del CU01

--- Desarrollo/Salvame/IU-04.js
+++ Desarrollo/Salvame/IU-04.js
@@ -5,6 +5,7 @@ const pasosFormulario = document.querySelectorAll(".paso-formulario");
 const pasosProgreso = document.querySelectorAll(".paso-progreso");
 
 let cantidadPasos = 0;
+let ultimalectura = 0;
 
 botonSiguiente.forEach((boton) => {
     boton.addEventListener("click", () => {
@@ -23,17 +24,24 @@ botonAnterior.forEach((boton) => {
 });
 
 function actualizarPasosFormulario() {
-    pasosFormulario.forEach((paso) => {
-        paso.classList.contains("paso-formulario-activo") &&
+    /*pasosFormulario.forEach((paso) => {
+        paso.classList.contains("paso-formulario-activo");
         paso.classList.remove("paso-formulario-activo");
     });
-
-    pasosFormulario[cantidadPasos].classList.add("paso-formulario-activo");
+    */
+    if (cantidadPasos > ultimalectura) {
+        pasosFormulario[cantidadPasos - 1].classList.remove("paso-formulario-activo");
+        pasosFormulario[cantidadPasos].classList.add("paso-formulario-activo");
+    } else {
+        pasosFormulario[cantidadPasos].classList.add("paso-formulario-activo");
+        pasosFormulario[cantidadPasos + 1].classList.remove("paso-formulario-activo");
+    }
+    ultimalectura = cantidadPasos;
 }
 
 function actualizarBarraProgreso() {
     pasosProgreso.forEach((pasosProgreso, indice) => {
-        if(indice < cantidadPasos +1) {
+        if (indice < cantidadPasos + 1) {
             pasosProgreso.classList.add("paso-progreso-activo");
         } else {
             pasosProgreso.classList.remove("paso-progreso-activo");
