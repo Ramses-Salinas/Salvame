d9fb5bb2a77f6e1b49d3589f1262da77c7b8b7ae
FabricioRosas <110144414+FabricioRosas@users.noreply.github.com>
2022-10-24 02:51:32 -0500

    Sesiones, postulacion a moderador y alertas
    -Se modifico la estructura del respositorio para una mejor organización.
    -Se concluyo la configuración de la conexión a la base de datos.
    - Se termino el funcionamiento de sesiones activas en la web, iniciar sesión de usuarios cerrar sesión y registrar nuevos usuarios.
    - Se avanzo la conexión del modulo de alerta, solo los ref no funcionalidades de backend
    - Se avanzao la conexión del modulo de postulantes a moderadores, solo ref no bakcend aun.
    FALTA:
    -Terminar una estructura optima de carpetas
    -Configuracion de los forms de alertas y postulaciones.

---
+++ Desarrollo/proyecto/IU-10.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="imagenes/logo_colores.jpg" />
+    <link rel="stylesheet" href="estilos/estiloIU-10.css" />
+    <title>Datos Personales</title>
+    <script src="https://code.jquery.com/jquery-3.6.1.js"
+        integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="ventana">
+        <div class="parte-1">
+            <h2>| Sálvame</h2>
+            <a href="#">Inicio</a>
+        </div>
+        <div class="parte-2">
+            <h3>DATOS PERSONALES</h3>
+            <form action="">
+                <input type="text" name="" id="user" placeholder="✉️ Correo electrónico" />
+                <select name="" id="">
+                    <option value=""> 📄Tipo de documento</option>
+                    <option value="DNI">DNI</option>
+                    <option value="Carnet">Carnet de extranjería</option>
+                </select>
+                <input type="text" name="" id="dni" placeholder="📄 Número de documento" />
+                <button type="button" id="buscar" class="btn btn-dark">Buscar</button>
+                <input type="text" name="" id="Nombres" />
+                <input type="text" name="" id="Apellidos" />
+                <br />
+                <input id="checkbox-1" type="checkbox" />
+                <label for="checkbox-1">He leído y acepto los términos y condiciones de “Sálvame”</label>
+                <br />
+                <input id="checkbox-2" type="checkbox" />
+                <label for="checkbox-2">Quiero recibir notificaciones de las alertas e información
+                    relevante de la página</label>
+                <br />
+                <input type="submit" value="Añadir" />
+            </form>
+        </div>
+    </div>
+</body>
+<script>
+$('#buscar').click(function() {
+    dni = $('#dni').val();
+    $.ajax({
+        type: 'post',
+        url: 'controlador/consultadni.php',
+        data: 'dni=' + dni,
+        dataType: 'json',
+        success: function(r) {
+            if (r.numeroDocumento == dni) {
+                $('#Apellidos').val(r.apellidoPaterno + " " + r.apellidoMaterno);
+                $('#Nombres').val(r.nombres);
+            } else {
+                alert('error');
+            }
+        }
+    });
+});
+</script>
+
+</html>
