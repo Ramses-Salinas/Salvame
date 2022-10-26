f543f74148204e8dec4bfb68561793afe59d2c8b
FabricioRosas11 <fabricio.rosas@unmsm.edu.pe>
2022-10-26 11:52:24 -0500

    Subida archivos y coordenadas
    Se completo la subida de archivos y coordenadas en el modulo de alerta

--- Desarrollo/Salvame/Implementación/moduloAlerta.php
+++ Desarrollo/Salvame/Implementación/moduloAlerta.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
-
 require './conexion_bd.php';
-
 if (isset($_SESSION['id_usuario'])) {
     //echo $_SESSION['id_usuario'];
     $records = $conn->prepare('SELECT id_usuario, correo, contrasenia FROM usuario WHERE id_usuario = :id_usuario');
@@ -14,6 +12,9 @@ if (isset($_SESSION['id_usuario'])) {
     if (count($results) > 0) {
         $user = $results;
     }
+}
+if (!empty($_POST["Anonima"])) {
+    echo '<script>console.log("boton anonimo")</script>';
 }
 // }
 ?>
@@ -27,6 +28,8 @@ if (isset($_SESSION['id_usuario'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos/estiloIU-04.css" />
+    <script src="https://code.jquery.com/jquery-3.6.1.js"
+        integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <title>Modulo Alerta</title>
 </head>
 
@@ -59,7 +62,7 @@ if (isset($_SESSION['id_usuario'])) {
     </header>
     <section>
         <h1>Módulo de Alerta</h1>
-        <form action="#" class="form">
+        <form action="./generaralertas.php" class="form" method="post" enctype="multipart/form-data">
             <!-- Barra de Progreso -->
             <div class="barraDeProgreso">
                 <div class="progreso" id="progreso"></div>
@@ -77,12 +80,12 @@ if (isset($_SESSION['id_usuario'])) {
                         <div class="nube-contenedor">
                             <img src="imagenes/nube.png" alt="">
                         </div>
-                        <div class="texto">Suelte el archivo aquí o
+                        <div class="texto" id="nombre_archivo">Suelte el archivo aquí o
                         </div>
                         <div class="boton-archivo">
                             <label for="Capturar">
                                 Capturar
-                                <input type="file" id="Capturar" />
+                                <input type="file" id="Capturar" name="Evidencia" />
                             </label>
                         </div>
                     </div>
@@ -98,7 +101,7 @@ if (isset($_SESSION['id_usuario'])) {
                     <form>
                         <section class="categoria">
                             <h3>Selecciona una categoria</h3>
-                            <select class="categoria" id="categoria">
+                            <select class="categoria" id="categoria" name="categoria">
                                 <option>Opción 1</option>
                                 <option>Opción 2</option>
                                 <option>Opción 3</option>
@@ -118,10 +121,10 @@ if (isset($_SESSION['id_usuario'])) {
                                     <p>Describir los hechos <img id="imagen2" src="imagenes/escribir.png" alt=""></p>
                                 </div>
                                 <div class="campoDes">
-                                    <textarea rows="15" maxlength="500" cols="40"></textarea>
+                                    <textarea rows="15" maxlength="500" cols="40" name="dec_hechos"></textarea>
                                 </div>
                                 <div class="campoDes">
-                                    <textarea rows="15" cols="40" maxlength="500"></textarea>
+                                    <textarea rows="15" cols="40" maxlength="500" name="dec_animal"></textarea>
                                 </div>
                                 <img class="fondoDiv" src="imagenes/fondo.png" alt="">
                             </section>
@@ -138,7 +141,103 @@ if (isset($_SESSION['id_usuario'])) {
                 <div class="contenedor" id="contenedor3">
                     <section class="seccion3" id="mapa">
                         <div id="mapDiv"></div>
-                        <script src="./js/map.js"></script>
+                        <script>
+                        let map;
+                        let marker;
+                        let watchID;
+                        let geoLoc;
+                        let latitud;
+                        let longitud;
+
+                        //const botonAnterior = document.querySelectorAll(".boton-capturar");
+                        function initMap() {
+                            const LatLng = {
+                                lat: -12.034662170145047,
+                                lng: -77.04546861557782
+                            }
+                            map = new google.maps.Map(document.getElementById("mapDiv"), {
+                                center: LatLng,
+                                zoom: 10,
+                            });
+                            marker = new google.maps.Marker({
+                                position: LatLng,
+                                map,
+                                draggable: true,
+                                title: "Marcador prueba"
+                            });
+                            getPosition();
+                            // javascript_to_php();
+                        }
+
+                        function getPosition() {
+                            //si el navegador soporta la geolocalizacion
+                            if (navigator.geolocation) {
+                                //cantidad de milisegundos en la que se tarda para ejecutar la funcion asiganda
+                                var options = {
+                                    tiemout: 60000
+                                };
+                                //devuelve un objeto geolocation que proporciona acceso web a la ubciacion del dispositivo
+                                geoLoc = navigator.geolocation;
+                                //watchPosition ejecuta la funcion asiganda showLocationOnMap 
+                                watchID = geoLoc.watchPosition(showLocationOnMap, errorHandler, options);
+                            } else {
+                                alert("El navegador no soporta la geolocalización");
+                            }
+                        }
+
+                        function showLocationOnMap(position) {
+                            latitud = position.coords.latitude;
+                            longitud = position.coords.longitude;
+                            console.log("latitud: " + latitud + "Longitud: " + longitud);
+
+                            const LatLng = {
+                                lat: latitud,
+                                lng: longitud
+                            };
+                            marker.setPosition(LatLng);
+                            map.setCenter(LatLng);
+                        }
+
+                        function errorHandler() {
+                            if (errorHandler.code == 1) {
+                                alert("Error: Acceso denegado");
+                            } else if (errorHandler.code == 2) {
+                                alert("Error: No se ecnuentra la posicion");
+
+                            }
+                        }
+
+                        function js_to_php_coords() {
+                            latitud = marker.getPosition().lat();
+                            longitud = marker.getPosition().lng();
+                            $(document).ready(function() {
+                                var latp = latitud;
+                                var lonp = longitud;
+                                //alert(latitud + longitud);
+                                $("#dibujo").load("generaralertas.php", {
+                                    latitud,
+                                    longitud
+                                });
+                            });
+                        }
+                        window.initMap = initMap;
+                        document.addEventListener("DOMContentLoaded", function() {
+                            function capturarUbicación() {
+                                latitud = marker.getPosition().lat();
+                                longitud = marker.getPosition().lng();
+                                var cadena = `Latitud: ${latitud}<br>Longitud: ${longitud}`;
+                                document.getElementById("resultado-ubicacion").innerHTML =
+                                    `<p>${cadena}</p>`;
+                            }
+
+                            document.getElementById("dibujo").onclick = function() {
+                                capturarUbicación();
+                                js_to_php_coords();
+
+                            }
+                        })
+                        </script>
+
                         <script async
                             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBFwt7lkRXa-tIUzTvzxhUlEq5nmHO_Ui0&callback=initMap"
                             defer>
@@ -147,10 +246,15 @@ if (isset($_SESSION['id_usuario'])) {
                     <section class="seccion3" id="opcionesMapa">
                         <article class="opcionesMap">
                             <p>Ubicación </p>
-                            <p style="color:#FCFCFC"><b>Resultado ubicación</b></p>
+                            <p id="resultado-ubicacion" style="color:#FCFCFC"><b>Resultado ubicación</b></p>
                         </article>
                         <article class="opcionesMap">
                             <a href="#" id="capturarUbicacion">
+                                <form action="generaralertas.php" method="POST">
+                                    <input type="text" name="latitud" id="latitud" value="">
+                                    <input type="text" name="longitud" id="longitud" value="">
+
+                                </form>
                                 <div id="dibujo">
                                     <div class="dibujo" id="arriba"> </div>
                                     <div class="dibujo" id="centro"></div>
@@ -164,7 +268,11 @@ if (isset($_SESSION['id_usuario'])) {
                 </div>
                 <div class="botones">
                     <a href="#" class="boton boton-anterior">Anterior</a>
-                    <a href="#" class="boton boton-siguiente">Siguiente</a>
+                    <a href="#" class="boton boton-siguiente">Siguiente
+                        <!-- <input name="btncoord" class="btn" type="submit"
+                            value="Siguiente" formaction="moduloAlerta.php"> -->
+                    </a>
+
                 </div>
             </div>
             <!-- Paso 4  -->
@@ -177,14 +285,14 @@ if (isset($_SESSION['id_usuario'])) {
                             </div>
 
                             <div class="eleccion">
-                                <a class="boton" id="priv" href="./index.php">
-                                    <p>Anónima</p>
-                                </a>
+                                <p><input type="submit" value="Anonima" name="Anonima" id="Anonima"> Anonima</p>
+                                <?php
+                                echo '<script>alert:("entro a guardar alerta");s</script>' ?>
                             </div>
 
                             <div class="eleccion">
-                                <a class="boton" id="priv" href="./IU-10.php">
-                                    <p>Pública</p>
+                                <a class="btn" id="priv" href="./IU-10.php">
+                                    <p><input type="submit" value="Publica">Pública</p>
                                 </a>
                             </div>
                             </a>
@@ -243,6 +351,14 @@ if (isset($_SESSION['id_usuario'])) {
         <p>© 2022 Sálvame. ALL RIGHT RESERVED.</p>
     </footer>
     <script src="./js/IU-04.js" defer></script>
+
+    <script type="text/javascript">
+    let archivo = document.querySelector('#Capturar');
+    archivo.addEventListener('change', () => {
+        document.querySelector('#nombre_archivo').innerText = archivo.files[0].name;
+    });
+    </script>
+
 </body>
 
 </html>
