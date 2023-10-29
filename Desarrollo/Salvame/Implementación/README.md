30ffdc06b27adbf12c345256d04e26e9854ea910
Stefano Hernández <95949020+LKA102@users.noreply.github.com>
2023-10-28 19:05:42 -0500

    Delete Desarrollo directory

--- Desarrollo/Salvame/Implementación/README.md
+++
@@ -1,2 +0,0 @@
-# Desarrollo
-Carpeta correspondiente a los proyectos de desarrollo que se llevan o han llevado a cabo
--- Desarrollo/Salvame/Implementación/js/captura.js
+++
@@ -1,9 +0,0 @@
-
-
-function capturarUbicación() {
-    alert("a");
-    document.getElementById("resultado-ubicacion").innerHTML = "latitud" + lat + "longitud" + lng;
-}
-document.getElementById("dibujo").onclick = function () {
-    capturarUbicación();
-}
